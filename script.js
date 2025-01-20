5babba0ad10ce24de230eefcbc3d6c08bffb0c04
tanneragle <tanneragle@gmail.com>
2025-01-20 16:37:16 -0500

    Setup game and round functions with input prompt from user repeating through five rounds.

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 // START Game Function "playGame"
-
+function playGame() {
 
     // INITIALIZE humanScore and computerScore to zero
-
+    let humanScore = 0;
+    let computerScore = 0;
 
     // START Round Function
-
+    function playRound() {
         // GET player's choice via Prompt
+        let humanChoice = prompt("Please enter your choice: Rock, Paper, or Scissors:").toLowerCase();
 
         // CHECK if valid, if not, EXIT round
+        let choices = ["rock", "paper", "scissors"];
+        humanChoice = choices.indexOf(humanChoice);
+        if (humanChoice === -1) {
+            console.log("Invalid Response. My instructions were very simple. Do not test me. Refresh the page and enter Rock, Paper, or Scissors.");
+            return; // Exit round if the input is invalid
+        }
 
         // GET computer's random choice 
 
@@ -20,13 +28,19 @@
 
 
     // END Round Function
-
+    }
     // Loop Five (5) Rounds
+    for (let i = 0; i < 5; i++) {
+        playRound(); // Call playRound function for each of the 5 rounds
+    }
 
     //  COMPARE humanScore and computerScore
 
     // OUTPUT final scores and DECLARE winner
 
-    
-// END Game Function
 
+// END Game Function
+}
+
+// Call playGame to start the game
+playGame();
