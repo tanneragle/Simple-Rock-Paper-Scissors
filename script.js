482d0e561f26f953924e0aba60ac7b9ce433e666
tanneragle <tanneragle@gmail.com>
2025-01-20 16:24:32 -0500

    Outline game logic in pseudocode comments.

---
+++ script.js
@@ -0,0 +1,32 @@
+// START Game Function "playGame"
+
+
+    // INITIALIZE humanScore and computerScore to zero
+
+
+    // START Round Function
+
+        // GET player's choice via Prompt
+
+        // CHECK if valid, if not, EXIT round
+
+        // GET computer's random choice 
+
+        // CALCULATE round result
+
+        // OUTPUT choices and result
+
+        // UPDATE scores (INCREMENT humanScore or computerScore, or no change)
+
+
+    // END Round Function
+
+    // Loop Five (5) Rounds
+
+    //  COMPARE humanScore and computerScore
+
+    // OUTPUT final scores and DECLARE winner
+
+    
+// END Game Function
+
